d6f0b8160b1b6de16b8b39e2bbd415cbc861cd16
josephsteenhuisen <110561019+josephsteenhuisen@users.noreply.github.com>
2023-03-12 13:36:02 -0400

    Create perfect_integer_lambda.h

---
+++ perfect_integer_lambda.h
@@ -0,0 +1,9 @@
+#ifndef __PERFECT_INTEGER__
+#define __PERFECT_INTEGER__
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+std::vector<int> find_perfect_numbers(const std::vector<int> input_list);
+
+#endif
